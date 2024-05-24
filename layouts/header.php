bfd771fe2e79d56e50c36bb2f72f1dd00f004d33
RichirastaHS <richirast19@gmail.com>
2024-05-23 20:19:00 -0600

    Estilos de la pp

--- layouts/header.php
+++ layouts/header.php
@@ -1,4 +1,13 @@
 <header>
-    <h1 class="Mono-titulos">WorkFlow</h1>
-    <a href="" class="RFlex">Iniciar sesion</a>
-</header> 
+        <h1 class="Mono-titulos">WorkFlow</h1>
+        <div >
+            <?php 
+                if (!isset($_SESSION['nombre'])) {
+                    echo "<a href='' class='RFlex'>Iniciar sesion</a>";
+                } else {
+                    echo "<a href='' class='RFlex gap20'> Administrar</a>";
+                    echo "<a href='' class='RFlex'>Cerrar sesión</a>";
+                }
+            ?>            
+        </div>
+    </header> 
